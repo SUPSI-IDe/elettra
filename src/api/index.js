a1fff4b97fa75deb3c9ae56ccbbdba80e9c01936
lucadra <luca.draisci@gmail.com>
2025-12-15 11:19:27 +0100

    refactor: split bus and bus model in two js files for easier maintenance

--- src/api/index.js
+++ src/api/index.js
@@ -2,6 +2,7 @@ export * from "./client";
 export * from "./auth";
 export * from "./user";
 export * from "./buses";
+export * from "./bus-models";
 export * from "./depots";
 export * from "./shifts";
 export * from "./gtfs";
