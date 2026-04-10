6af3616e94b1c9de6f405a82f920353db2b56b94
dstreppa <davide.strepparava@supsi.ch>
2026-04-10 16:19:35 +0200

    Added parameters for ebus with diesel heating

--- src/config/auxiliary-consumption-defaults.js
+++ src/config/auxiliary-consumption-defaults.js
@@ -10,4 +10,13 @@ export const AUXILIARY_CONSUMPTION_KW_DEFAULTS = {
     consumption_kw: [24, 16, 12, 8, 9, 10, 16],
     temperature_celsius: [-5, 0, 5, 10, 15, 20, 25],
   },
+  diesel: {
+    t_ref_celsius: 10,
+    p_base_kw: 8,
+    cop: {
+      temperature_celsius: [-10, -5, 0, 5, 10],
+      values: [2.0, 2.2, 2.5, 2.7, 3.0],
+    },
+    diesel_heater_efficiency: 0.83,
+  },
 };
